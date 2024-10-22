62bc3fef835b366f30236cfe1795dacb8dd7d226
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-22 15:55:56 +0600

    user model updated

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -36,9 +36,6 @@ userSchema.pre("save", async function(next) {
   next();
 })
 
-userSchema.methods.isPasswordCorrect = async function (password) {
-  return await bcrypt.compare(password, this.password);
-}
 
 userSchema.methods.generateAccessToken = function(){
   return jwt.sign(
